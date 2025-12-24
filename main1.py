ab4ad0669863d184deccc57c559c237df2a77a49
jainever20 <jainever20@gmail.com>
2025-12-24 17:40:59 +0530

    added endpoint delete&update

--- main1.py
+++ main1.py
@@ -4,6 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 import os
 from dotenv import load_dotenv
+from typing import Optional
 
 load_dotenv()
 
@@ -37,3 +38,36 @@ async def get_euron_data():
     async for document in cursor:
         items.append(doc_helper(document))
     return items
+
+@app.delete("/euron/delete/{id}")
+async def delete_records(id:str):
+    result = await euron_data.delete_one({"_id":ObjectId(id)})
+
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404,detail="Record not found")
+    
+    return {"message":"Record deleted successfully"}
+
+
+class UpdateData(BaseModel):
+    name: Optional[str] = None
+    phone: Optional[int] = None
+    city: Optional[str] = None
+    course: Optional[str] = None
+
+@app.put("/euron/update/{id}")
+async def update_records(id : str, data:UpdateData):
+    updated_data = {k:v for k,v in data.dict().items() if v is not None}
+
+    if not updated_data:
+        raise HTTPException(status_code=400,detail="No data provided to update")
+    
+    result = await euron_data.update_one(
+        {"_id":ObjectId(id)},
+        {"$set": updated_data}
+    )
+
+    if result.matched_count == 0:
+        raise HTTPException(status_code=404, detail="Record not found")
+
+    return {"message": "Record updated successfully"}
